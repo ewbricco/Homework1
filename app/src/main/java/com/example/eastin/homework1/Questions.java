40694bc7ae3ed5c69264a1fd8e1b45c02f46c926
Eastin <ewbricco@gmail.com>
2016-02-19 20:50:36 -0600

    Flawed Fragment implementation.

--- app/src/main/java/com/example/eastin/homework1/Questions.java
+++ app/src/main/java/com/example/eastin/homework1/Questions.java
@@ -1,14 +1,45 @@
 package com.example.eastin.homework1;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 public class Questions extends Activity {
+
+    int questionNumber = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_questions);
+        //setContentView(R.layout.activity_questions);
+        frags();
+
+
+
+
+    }
+
+    public void frags(){
+        FragmentManager fragmentManager = getFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if(questionNumber == 1){
+            question1 q1 = new question1();
+            fragmentTransaction.replace(android.R.id.content, q1);
+        }
+        else{
+            question2 q2 = new question2();
+            fragmentTransaction.replace(android.R.id.content, q2);
+        }
+
+        fragmentTransaction.commit();
+    }
+
+    public void gotoNextQuestion(){
+        questionNumber++;
+        frags();
     }
 
 }
---
+++ app/src/main/java/com/example/eastin/homework1/question2.java
@@ -0,0 +1,19 @@
+package com.example.eastin.homework1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Eastin on 2/19/2016.
+ */
+public class question2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.question2, container, false);
+    }
+}
