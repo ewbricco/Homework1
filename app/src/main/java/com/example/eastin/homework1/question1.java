73099459b43276d6619f7a6f6c2b608fd1775ea3
Eastin <ewbricco@gmail.com>
2016-02-19 21:14:35 -0600

    All activities added.

--- app/src/main/java/com/example/eastin/homework1/question1.java
+++ app/src/main/java/com/example/eastin/homework1/question1.java
@@ -1,6 +1,8 @@
 package com.example.eastin.homework1;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -17,4 +19,5 @@ public class question1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.question1, container, false);
     }
+
 }
